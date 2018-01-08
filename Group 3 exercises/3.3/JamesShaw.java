be6e259e65b4538eec57f83f26896f1c3a390806
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 20:12:31 +0000

    James Shaw Exercises

--- Group 3 exercises/3.3/JamesShaw.java
+++ Group 3 exercises/3.3/JamesShaw.java
@@ -9,33 +9,32 @@ class Main
 {
     public static void main( String args[] )
     {
-        String word = BIO.getString();
-		String newWord = word.toLowerCase();
+        String word = BIO.getString();							//set word as input
+	String newWord = "";
+	Boolean swapped;								//create other variables
 	
-        while (! "END".equals(word))
+        while (!word.equals("END"))							//while word does not equal "END"
         {
-            Boolean swapped = false;
-            for (int i = 1; i < newWord.length(); i++)
+            swapped = false;								//Set a boolean called swapped to false
+	    newWord = word.toLowerCase();						//set newWord as the lowercase version of word
+            for (int i = 1; i < newWord.length(); i++)					//for all values from 1 to newWord length -1
             {
-                if (newWord.charAt(i-1) > newWord.charAt(i))
+                if (newWord.charAt(i-1) > newWord.charAt(i))				//if character at position i has a higher ascii value than the character before it
                 {
-                    swapped = true;
+                    swapped = true;							//change swapped to true
+		    break;								//break out of the loop as you don't need to check anymore characters
                 }
             }
             
-            if (swapped == true)
+            if (swapped == true)							//if swapped is true
             {
-                System.out.printf("%-10s %-10s",word,"letters not in ascending order");
+                System.out.printf("%-10s %-10s",word,"letters not in ascending order");	//print that the letters are not in ascending order
             }
-            else
+            else									//if swapped is false
             {
-                System.out.printf("%-10s %-10s",word,"letters in ascending order");
+                System.out.printf("%-10s %-10s",word,"letters in ascending order");	//print that the  letters are in ascending order
             }
-            word = BIO.getString();
-			if (!"END".equals(word))
-			{
-				newWord = word.toLowerCase();
-			}
+            word = BIO.getString();							//get a new word
         }
     }
 }
