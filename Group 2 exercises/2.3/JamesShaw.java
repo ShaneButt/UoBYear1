f807e3ca96706ea72f4d21f113b66e2d5a25e862
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 19:05:51 +0000

    James Shaw Exercises

--- Group 2 exercises/2.3/JamesShaw.java
+++ Group 2 exercises/2.3/JamesShaw.java
@@ -9,30 +9,30 @@
 {
 	public static void main( String args[] )
 	{
-		String name = BIO.getString();
+		String name = BIO.getString();						//get name as input
 		int cw = 0;
 		int exam = 0;
 		double mark = 0;
-		String result = "";
-		while (!"END".equals(name))
+		String result = "";							//create other variables 
+		while (!name.equals("END"))						//while name does not equal "END"
 		{
-			cw = BIO.getInt();
-			exam = BIO.getInt();
-			mark = ((double)cw + (double) exam)/2;
-			if (mark >= 40 && cw >= 30 && exam >= 30)
+			cw = BIO.getInt();						//get coursework mark
+			exam = BIO.getInt();						//get exam mark
+			mark = ((double)cw + (double) exam)/2;				//calculate overall mark
+			if (mark >= 40 && cw >= 30 && exam >= 30)			//if mark is 40 or greater AND coursework is 30 or greater AND exam is 30 or greater
 			{
-				result = "Pass";
+				result = "Pass";					//result equals "Pass"
 			}
-			else if (mark >= 40 && (cw <= 30 || exam <= 30))
+			else if (mark >= 40 && (cw <= 30 || exam <= 30))		//if mark is 40 or greater and EITHER coursework or exam is 30 or greater
 			{
-				result = "Fail [Threshold]";
+				result = "Fail [Threshold]";				//result equals "Fail [Threshold]"
 			}
-			else
+			else								//if mark is less than 40 OR mark is greater than 40 but both exam and coursework are less than 30
 			{
-				result = "Fail";
+				result = "Fail";					//result equals "Fail"
 			}
-			System.out.print(name+" ["+cw+", "+exam+"] "+mark+" "+result);
-			name = BIO.getString();
+			System.out.print(name+" ["+cw+", "+exam+"] "+mark+" "+result);	//print the result
+			name = BIO.getString();						//get the next name (or END condition)
 		}
 	}
 }
