c0925d062bf1daf72de04f1d1ef9541df92a088c
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:14:07 +0000

    Update JamesShaw.java

--- Group 1 exercises/1.3/JamesShaw.java
+++ Group 1 exercises/1.3/JamesShaw.java
@@ -9,17 +9,17 @@
 {
     public static void main( String args[] )
     	{
-		int sn = BIO.getInt();
+		int sn = BIO.getInt();									//get student number as input
 		int cw = 0;
 		int exam = 0;
-		double mark = 0;
-		while (sn != 0 && String.valueOf(sn).length() == 8)
+		double mark = 0;									//create other variables
+		while (sn != 0 && String.valueOf(sn).length() == 8)					//While loop that says while sn does not equal 0 and the length of the student number is 8 characters, then run this code
 		{
-			cw= BIO.getInt();
-			exam = BIO.getInt();
-			mark = ((double)cw + (double)exam)/2;
-			System.out.println("sn = "+sn+" ex = "+exam+" cw = "+cw+" mark = "+mark);
-			sn = BIO.getInt();
+			cw= BIO.getInt();								//get coursework mark
+			exam = BIO.getInt();								//get exam mark
+			mark = ((double)cw + (double)exam)/2;						//calculate final mark
+			System.out.println("sn = "+sn+" ex = "+exam+" cw = "+cw+" mark = "+mark);	//print the summary to the terminal
+			sn = BIO.getInt();								//get the next student number or get the end value (0)
 		}
    	}
 }
