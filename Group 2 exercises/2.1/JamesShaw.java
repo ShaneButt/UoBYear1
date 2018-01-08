6d586ad8f77e28512818a75755ba1e10f4236a98
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:48:29 +0000

    James Shaw Exercises

--- Group 2 exercises/2.1/JamesShaw.java
+++ Group 2 exercises/2.1/JamesShaw.java
@@ -9,22 +9,22 @@
 {
     public static void main( String args[] )
     {
-        String name = BIO.getString();
+        String name = BIO.getString();		//get student name as input
         int mark = 0;
-        String result = "";
-        while (!"END".equals(name))
+        String result = "";			//create other variables with empty placeholder values
+        while (!name.equals("END"))		//while the variable name does not equal "END"
         {
-            mark = BIO.getInt();
-            if (mark >= 40)
+            mark = BIO.getInt();		//get student mark as input
+            if (mark >= 40)			//if the mark is greater than or equal to 40
             {
-                result = " Pass";
+                result = " Pass";		//result is equal to pass
             }
-            else
+            else				//if the result is less than 40
             {
-                result = " Fail";
+                result = " Fail";		//result is equal to fail
             }
-            System.out.print(name + result);
-            name = BIO.getString();
+            System.out.print(name + result);	//print the student's name and their result
+            name = BIO.getString();		//get the next student's name as input (or get END value)
         }
     }
 }
