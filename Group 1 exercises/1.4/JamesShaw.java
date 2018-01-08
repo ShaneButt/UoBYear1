2ac0a31b2a51abf876fe66dd13da829c841ed1d2
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:20:27 +0000

    Update JamesShaw.java

--- Group 1 exercises/1.4/JamesShaw.java
+++ Group 1 exercises/1.4/JamesShaw.java
@@ -9,14 +9,14 @@ class Main
 {
     public static void main( String args[] )
     {
-        int size = BIO.getInt();
-        for (int i = 0; i < size; i++)
+        int size = BIO.getInt();            //get size as input
+        for (int i = 0; i < size; i++)      //first loop used for the 'rows' - each iteration of the loop is a different row
         {
-            for (int j = 0; j < size; j++)
+            for (int j = 0; j < size; j++)  //second loop used for the 'columns' - each iteration of the loop is a different column
             {
-                System.out.print("*");
+                System.out.print("*");      //prints a * once for each 'column' within each 'row'
             }
-            System.out.println("");
+            System.out.println("");         //puts a new line at the end of each column - equivalent of pressing enter
         }
     }
 }
