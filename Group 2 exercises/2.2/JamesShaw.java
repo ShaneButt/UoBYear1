267d4ff02402571cc80c096935e6ca438b67f47a
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:58:23 +0000

    James Shaw Exercises

--- Group 2 exercises/2.2/JamesShaw.java
+++ Group 2 exercises/2.2/JamesShaw.java
@@ -9,34 +9,34 @@
 {
 	public static void main( String args[] )
 	{
-		String name = BIO.getString();
+		String name = BIO.getString();				//get name as input
 		int mark = 0;
-		String result = "";
-		while (!"END".equals(name))
+		String result = "";					//create other variables with empty placeholder values
+		while (!name.equals("END"))				//while name does not equal "END"
 		{
-			mark = BIO.getInt();
-			if (mark < 40)
+			mark = BIO.getInt();				//get mark as input
+			if (mark < 40)					//if mark is less than 40
 			{
-				result = "fails";
+				result = "fails";			//result equals "fails"
 			}
-			else if (mark < 50)
+			else if (mark < 50)				//if mark is greater than or equal to 40 but less than 50
 			{
-				result = "gains a 3rd";
+				result = "gains a 3rd";			//result equals "gains a 3rd"
 			}
-			else if (mark < 60)
+			else if (mark < 60)				//if mark is greater than or equal to 50 but less than 60
 			{
-				result = "gains a 2.2";
+				result = "gains a 2.2";			//result equals "gains a 2.2"
 			}
-			else if (mark < 70)
+			else if (mark < 70)				//if mark is greater than or equal to 60 but less than 70
 			{
-				result = "gains a 2.1";
+				result = "gains a 2.1";			//result equals "gains a 2.1"
 			}
-			else
+			else						//if mark is greater than or equal to 70
 			{
-				result = "gains a 1st";
+				result = "gains a 1st";			//result equals "gains a 1st"
 			}
-			System.out.print(name+" ["+mark+"] "+result);
-			name = BIO.getString();
+			System.out.print(name+" ["+mark+"] "+result);	//print the result
+			name = BIO.getString();				//get next name (or END value)
 		}
 	}
-}
+}	
