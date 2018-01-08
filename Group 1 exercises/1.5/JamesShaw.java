f470b3546701853e0dbd3450dce76dbce211404c
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:43:24 +0000

    James Shaw Exercises

--- Group 1 exercises/1.5/JamesShaw.java
+++ Group 1 exercises/1.5/JamesShaw.java
@@ -9,26 +9,28 @@
 {
      public static void main( String args[] )
      {
-        int size = BIO.getInt();
-        for (int i = 0; i < size; i++)
+        int size = BIO.getInt();		//get size as input
+        for (int i = 0; i < size; i++)		//first for loop used write each row
         {
-            if (i == 0 || i == (size - 1))
+            if (i == 0 || i == (size - 1))	//if the program is writing the first or last row 
+		    				//size-1 because the loop says while i<size so max value is size-1
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < size; j++)	//loop used for the columns of the first and last rows
                 {
-                    System.out.print("*");
+                    System.out.print("*");	//write *s
                 }
             }
-            else
+            else				//if not the first or last row
             {
-                System.out.print("*");
-                for(int k = 2; k < size; k++)
+                System.out.print("*");		//print 1 *
+                for(int k = 2; k < size; k++)	//loop used to fill in the gap between the first and last column
+						//note that the range of values in the for loop starts at 2 instead of 0, but still finishes at size-1. This will run the loop 2 less times
                 {
-                    System.out.print(".");
+                    System.out.print(".");	//write .s
                 }
-                System.out.print("*");
+                System.out.print("*");		//print the end *
             }
-            System.out.println("");
+            System.out.println("");		//new line
         }
      }
 }
