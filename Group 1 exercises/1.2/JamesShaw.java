f15454681868934414a9423e279e22521489f441
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2018-01-08 18:10:26 +0000

    Update JamesShaw.java

--- Group 1 exercises/1.2/JamesShaw.java
+++ Group 1 exercises/1.2/JamesShaw.java
@@ -12,6 +12,6 @@
         int cw = BIO.getInt();		//get coursework mark
         int exam = BIO.getInt();	//get exam mark
         double mark = ((double)cw + (double)exam)/2;	//change cw and exam from int to double, then work out the overall mark
-		System.out.print("ex = " + exam + " cw = " + cw + " mark = " + mark);
+	System.out.print("ex = " + exam + " cw = " + cw + " mark = " + mark);  //use print to send the final line to the terminal
     }
 }
