3774714bccb3bd3b8ecdeb4aa5c035ffb5d718a7
ShaneSloth <skeletalorigin@gmail.com>
2018-01-08 17:16:23 +0000

    Group 2 Exercises
    Shane Butt Exercises

--- Group 1 Exercises/1.5/ShaneButt.java
+++ Group 1 Exercises/1.5/ShaneButt.java
@@ -10,35 +10,41 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int num = BIO.getInt();
-        for (int x = 1; x <= num; x++)
+        int num = BIO.getInt(); // width integer
+        for (int x = 1; x <= num; x++) // intialise the column
         {
-            for (int y = 1; y <= num; y++)
+            for (int y = 1; y <= num; y++) // intialise the row (i inversed the variables as i wrote this)
             {
-                if (x == 1 || x == num)
+                if (x == 1 || x == num) // is it the first or last row?
                 {
-                    if (y == num)
+                    // if yes, then we need to make it *s, this is ebcause
+                    // this would be the outline
+                    if (y == num) // is it the end of that row?
                     {
-                        System.out.println("*");
+                        System.out.println("*"); // new line required after
                     }
                     else
                     {
-                        System.out.print("*");
+                        System.out.print("*"); // no? no new line needed, as you were
                     }
                 }
-                else
+                else // not the first or last row...
                 {
-                    if (y == num)
+                    if (y == num) // end of the row
                     {
-                        System.out.println("*");
+                        System.out.println("*"); // still requires a new line and last index
+                        // of the row would be outline, therefore *.
                     }
                     else if (y == 1)
                     {
-                        System.out.print("*");
+                        System.out.print("*"); // first index is outline, therefore *
+                        // no new line needed
                     }
                     else
                     {
-                        System.out.print(".");
+                        System.out.print("."); // if it is not the first or last index
+                        // it is not the outline, and therefore it is the body
+                        // fill it with full stops bois
                     }
                 }
 
