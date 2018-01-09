e4936fbbb7624d56e2be2eb1e193440c40f095fc
Elijah Murray <34821469+murray-elijah@users.noreply.github.com>
2018-01-09 09:10:35 +0000

    Update 2.java

--- Group 1 - Eli/2.java
+++ Group 1 - Eli/2.java
@@ -1,8 +1,17 @@
+/**
+ * Eli Murray - ejdm11
+ */
+
 public class Main {
 	public static void main(String[] args) {
-		double cw = BIO.getInt();
-		double ex = BIO.getInt();
+		double cw = BIO.getInt(); //Coursework mark
+		double ex = BIO.getInt(); //Exam mark
 		double mark = ( (cw + ex)/2);
 		System.out.println("ex = "+(int)ex+" cw = "+(int)cw+" mark = "+mark);
+		/**
+		 * 
+		 * Please note: The use of (int) in front of ex and cw on line 10 are for
+		 * casting (converting on the fly) the variables to an integer
+		 */
 	}
 }
