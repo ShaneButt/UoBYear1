1734d8b1c45410f9af8dae56ab4d3a6ab6f643b5
ShaneSloth <skeletalorigin@gmail.com>
2018-01-09 09:56:58 +0000

    Shane But Exercises
    Documented 2.5

--- Group 2 Exercises/2.5/ShaneButt.java
+++ Group 2 Exercises/2.5/ShaneButt.java
@@ -14,73 +14,90 @@
 
 class Main
 {
-    private static int NumCoins;
-    private static int amount;
-    private static int psuedoAmount;
-    private static String[] strings = new String[8];
-    private static int[] coins = {200, 100, 50, 20, 10, 5, 2, 1};
+    /*
+        here we set up our variables for the entire class
+        'private' means they can only be used by the parent class
+        we create these here so that they are shared and so we do not have to recreate or
+            parse them through methods unless necessary,
+            this is because they are accessible and are changed when needed
+    */
+    private static int NumCoins; // the number of coins used
+    private static int amount; // the amount inputted
+    private static int psuedoAmount; // the amount we do maths on
+    private static String[] strings = new String[8]; // an empty string array of length 8 (0-7)
+    private static int[] coins = {200, 100, 50, 20, 10, 5, 2, 1}; // an integer array containing the coins we will use
 
     public static int numCoins(int coin, int dummyAmount)
     {
         return (int) Math.ceil(dummyAmount/coin);
-    }
+    } // this returns the number of coins in a given amount of change by taking the amount, 'dummyAmount' and dividing by the coin
 
-    public static String buildString(String[] array)
+
+    public static String buildString(String[] array) // note the return type 'String' rather than void.
     {
-        String built = "";
-        for (int i = 0; i < array.length; i++)
+        String built = ""; // create an empty string to work with, 'built'
+        for (int i = 0; i < array.length; i++) // run through the array from the first to last index
         {
-	    if (array[i] != "")
-	    {
-             	built = built + array[i] + " ";
-	    }
+	    if (array[i] != "") // if the element in the i'th index of the array is not empty
+	        {
+	            built = built + array[i] + " "; // we then add (concatenate) whatever 'built' contains
+                // with whatever the array contains at the i'th index and then add a space at the end
+	        }
         }
-	built = String.format("%-1s", built);
-        return built;
+	    built = String.format("%-1s", built); // we then format our string
+        return built; // return the string
     }
     
-    public static void doMaths(String str, int amount, int coinValue, int index)
+    public static void doMaths(String str, int amount, int coinValue, int index) // note the 'void' return type (meaning it doesnt return a value)
     {
-        int pVar;
-        String sVar;
-        pVar = numCoins(coinValue, amount);
-        psuedoAmount = amount - (pVar * coinValue);
+        int pVar; // intialise an integer variable to work with
+        String sVar; // intialise an empty string variable to work with
+        pVar = numCoins(coinValue, amount); // change pVar to represent the number of coins
+        psuedoAmount = amount - (pVar * coinValue); // take the amount and subtract how much of each coin we just used
+        /* Example
+        * Str = "100p";
+        * Amount = 232;
+        * CoinValue = 100;
+        * pseudoAmount  = 132 - (2 * 100);
+        *               = 32;
+        * this value is then changed *globally* as it is shared through every method contained within the parent class
+        */
         if (pVar>1) { sVar = String.format("%d*%s",pVar, str); } else if (pVar==1) { sVar = str; } else { sVar = ""; }
-        strings[index] = sVar;
-        NumCoins= NumCoins + pVar;
+        strings[index] = sVar; // insert the string we just formatted
+        // sVar would be along the lines of (following our previous example:
+        // "2*100p";
+        // this is because it is formatted as %d * %s (where %d is an integer format, 2, and %s is a string format, 100p
+        NumCoins= NumCoins + pVar; // we then add the number of coins used to the global number of coins.
     }
 
     public static void main(String args[])
     {
-        amount = BIO.getInt();
-        psuedoAmount = amount;
-	    System.out.printf("%s %-2s\n", "Amount", "Coins");
-        while (amount!=0)
+        amount = BIO.getInt(); // set the global 'amount' a value
+        psuedoAmount = amount; // set our mathematical amount to the amount
+	    System.out.printf("%s %-2s\n", "Amount", "Coins"); // first print required
+        while (amount!=0) // terminator is when the amount is 0
         {
-            if (amount < 1 || amount > 500)
+            if (amount < 1 || amount > 500) // these are the set rules for the program
             {
-                System.out.println("Invalid amount " + amount + "p");
-				amount = BIO.getInt();
-				psuedoAmount = amount;
-				NumCoins = 0;
+                System.out.println("Invalid amount " + amount + "p"); // output its invalid
+				amount = BIO.getInt(); // reset amount
+				psuedoAmount = amount; // reset psuedo
+				NumCoins = 0; // reset NumCoins;
             }
             else
             {
-                int p200, p100, p50, p20, p10, p5, p2, p1;
-                String s200, s100, s50, s20, s10, s5, s2, s1;
-                
-                for (int i = 0; i < coins.length; i++)
+                for (int i = 0; i < coins.length; i++) // iterate through our coins array
                 {
-                    doMaths(Integer.toString(coins[i])+"p", psuedoAmount, coins[i], i);
+                    doMaths(Integer.toString(coins[i])+"p", psuedoAmount, coins[i], i); // follow doMaths method
                 }
             
-            	String s = buildString(strings);
-            	System.out.printf("%3dp", amount);
-	    	if (NumCoins>0 && NumCoins < 2) { System.out.printf("%d coin  ", NumCoins); } else { System.out.printf("%4d coins  ", NumCoins); }
+            	String s = buildString(strings); // build our string to output
+            	System.out.printf("%3dp", amount); // first part of our line is the amount of change
+	    	    if (NumCoins>0 && NumCoins < 2) { System.out.printf("%d coin  ", NumCoins); } else { System.out.printf("%4d coins  ", NumCoins); }
             	System.out.printf("%s", s);
             	amount = BIO.getInt();
-	    	psuedoAmount = amount;
-	    	NumCoins = 0;
+	    	    psuedoAmount = amount;
+	    	    NumCoins = 0;
 	    }
         }
     }
