f2b907c71d3d86e45bb1f85b1897b14b30c6b348
Elijah Murray <34821469+murray-elijah@users.noreply.github.com>
2018-01-09 09:16:14 +0000

    Update 1.java

--- Group 2 - Eli/1.java
+++ Group 2 - Eli/1.java
@@ -1,13 +1,17 @@
+/**
+ * Eli Murray - ejdm11
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	public static List<String> names = new ArrayList<String>();
-	public static List<String> scores = new ArrayList<String>();
+	public static List<String> names = new ArrayList<String>();	// Stores the names and
+	public static List<String> scores = new ArrayList<String>();	// scores in ArrayLists
 
 	public static void main(String[] args) {
-		String name = "";
-		int score = 0;
+		String name = "";					// These two variables store the
+		int score = 0;						// current name and score
 
 		while(true) {
 			name = BIO.getString();
@@ -15,20 +19,20 @@ public class Main {
 				print();
 			}
 			score = BIO.getInt();
-			names.add(name);
+			names.add(name);				// Add name to the names ArrayList declared line 9
 
 			if(score < 40) {
 				scores.add(" Fail");
 			} else {
 				scores.add(" Pass");
-			}
-		}
+			}						// Push " Fail" or " Pass" to the
+		}							// scores ArrayList declared line 10
 	}
 
 	private static void print() {
 		for (int i = 0; i < names.size(); i++) {
-			System.out.println(names.get(i) + scores.get(i));
-		}
+			System.out.println(names.get(i) + scores.get(i));	// ___.get pulls the value at index
+		}								// i from each ArrayList
 		System.exit(1);
 	}
 }
